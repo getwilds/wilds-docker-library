e41679c00b16c700f7d0acdfd22e423f3ac87627
tefirman <tefirman@gmail.com>
2025-10-13 20:55:21 -0700

    Adjusting GitHub Actions to utilize uv script dependency metadata

--- .github/scripts/commit_cve_reports.py
+++ .github/scripts/commit_cve_reports.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
-# -*-coding:utf-8 -*-
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = "==3.13"
+# dependencies = ["pyyaml==6.0.3", "gitpython==3.1.45", "requests==2.32.5"]
+# ///
 """
 @File    :   commit_cve_reports.py
 @Time    :   2025/06/20 11:34:12
